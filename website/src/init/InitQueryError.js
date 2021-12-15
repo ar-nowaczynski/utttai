f32062f767610251486d6b049044ddc77ed4e719
ar-nowaczynski <ar.nowaczynski@gmail.com>
2021-12-15 13:43:29 +0100

    Add Init Query Error title

--- website/src/init/InitQueryError.js
+++ website/src/init/InitQueryError.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 class InitQueryError extends React.PureComponent {
   render() {
     const { initQueryError } = this.props;
     return (
       <div className="init-query-error">
+        <Helmet defer={false}>
+          <title>uttt.ai – Init Query Error</title>
+        </Helmet>
         <div className="init-query-error-title">Init Query Error</div>
         <div className="init-query-error-message">{initQueryError.message}</div>
         <div className="init-query-error-url">
